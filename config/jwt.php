ae3105171b0f03177f6818479e7ea68d573ff244
Capocasale Romain <romain.capocasa@hes-so.ch>
2020-09-28 14:18:27 +0200

    add composer.lock

--- config/jwt.php
+++ config/jwt.php
@@ -21,7 +21,7 @@ return [
     |
     */
 
-    'secret' => env('JWT_SECRET', 'BvjpTAv86dMLdZdrGRSW6GFY9kbJXI3R'),
+    'secret' => env('JWT_SECRET', 'MuNLT34B3UFyT9lkbgUMN9Crit0ZytBu'),
 
     /*
     |--------------------------------------------------------------------------
